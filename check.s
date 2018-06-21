8d8d4fef5ebcd6600f7f2722f3e0262ee8c3a593
nyo <vidarr.vali@gmail.com>
2018-06-21 18:22:03 -0300

    nothing much yet

---
+++ check.s
@@ -0,0 +1,17 @@
+# Function that checks whether the selected piece can eat
+# if it can eat, then it creater an array on the stack with possible moves
+# if it can't eat, it must be able to move
+# if it can move, then it creates an array on the stack with possible moves
+# if it can't move, the game ends because that shouldn't be possible
+
+# s0 = initial address of the board
+# s1 = final address of the board
+
+# arguments
+# a0 = address of the piece on the board
+# a1 = type of the piece
+
+.data
+
+.text
+
